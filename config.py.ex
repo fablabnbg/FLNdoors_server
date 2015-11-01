9d73e7c86eb7ad8f2745f03496edb9a37909bc4b
Patrick Himmelmann <himmelmannp@gmail.com>
2015-11-01 01:19:08 +0100

    Read config

---
+++ config.py.ex
@@ -0,0 +1,2 @@
+listen_addr='192.168.0.1'
+listen_port=80
--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from app_index import App_index
 from app_auth import App_auth
 from app_status import App_status
 
+import config
+
 apps={
 	None:App_index, #default
 	"auth":App_auth,
@@ -23,5 +25,5 @@ def select_app(environ,start_response):
 	return active_app(environ,start_response)
 
 if __name__=='__main__':
-	s = make_server('', 80, select_app)
+	s = make_server(config.listen_addr, config.listen_port, select_app)
 	s.serve_forever()
